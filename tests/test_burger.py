b450f43d304c331c271392f10953a9cfb5ffc9dd
Margarita Khokhlova <khokhlova2105@yandex.ru>
2025-03-19 17:09:18 +0300

    2 commit

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -1,11 +1,12 @@
 from praktikum.burger import Burger
 from unittest.mock import Mock
+from data import Data
 
 class TestBurger:
     def test_set_buns(self):
         burger = Burger()
         mock_bun = Mock()
-        mock_bun.get_name.return_value = 'black bun'
+        mock_bun.get_name.return_value = Data.bun_name
         burger.set_buns(mock_bun)
 
         assert mock_bun.get_name() == burger.bun.get_name()
@@ -13,9 +14,9 @@ class TestBurger:
     def test_add_ingredient(self):
         burger = Burger()
         mock_ingredient = Mock()
-        mock_ingredient.get_type.return_value = 'SAUCE'
-        mock_ingredient.get_name.return_value = 'hot sauce'
-        mock_ingredient.get_price.return_value = 20
+        mock_ingredient.get_type.return_value = Data.ingredient_type
+        mock_ingredient.get_name.return_value = Data.ingredient_name
+        mock_ingredient.get_price.return_value = Data.ingredient_price
         burger.add_ingredient(mock_ingredient)
 
         assert len(burger.ingredients) == 1
@@ -23,9 +24,9 @@ class TestBurger:
     def test_remove_ingredient(self):
         burger = Burger()
         mock_ingredient = Mock()
-        mock_ingredient.get_type.return_value = 'SAUCE'
-        mock_ingredient.get_name.return_value = 'hot sauce'
-        mock_ingredient.get_price.return_value = 20
+        mock_ingredient.get_type.return_value = Data.ingredient_type
+        mock_ingredient.get_name.return_value = Data.ingredient_name
+        mock_ingredient.get_price.return_value = Data.ingredient_price
         burger.add_ingredient(mock_ingredient)
         burger.remove_ingredient(0)
 
@@ -35,12 +36,12 @@ class TestBurger:
         burger = Burger()
         mock_ingredient = Mock()
         mock_ingredient_2 = Mock()
-        mock_ingredient.get_type.return_value = 'SAUCE'
-        mock_ingredient.get_name.return_value = 'hot sauce'
-        mock_ingredient.get_price.return_value = 20
-        mock_ingredient_2.get_type.return_value = 'FILLING'
-        mock_ingredient_2.get_name.return_value = 'cutlet'
-        mock_ingredient_2.get_price.return_value = 10
+        mock_ingredient.get_type.return_value = Data.ingredient_type
+        mock_ingredient.get_name.return_value = Data.ingredient_name
+        mock_ingredient.get_price.return_value = Data.ingredient_price
+        mock_ingredient_2.get_type.return_value = Data.ingredient_type_2
+        mock_ingredient_2.get_name.return_value = Data.ingredient_name_2
+        mock_ingredient_2.get_price.return_value = Data.ingredient_price_2
         burger.add_ingredient(mock_ingredient)
         burger.add_ingredient(mock_ingredient_2)
         burger.move_ingredient(0, 1)
@@ -50,12 +51,12 @@ class TestBurger:
 
     def test_get_price(self):
         mock_ingredient = Mock()
-        mock_ingredient.get_type.return_value = 'SAUCE'
-        mock_ingredient.get_name.return_value = 'hot sauce'
-        mock_ingredient.get_price.return_value = 20
+        mock_ingredient.get_type.return_value = Data.ingredient_type
+        mock_ingredient.get_name.return_value = Data.ingredient_name
+        mock_ingredient.get_price.return_value = Data.ingredient_price
         mock_bun = Mock()
-        mock_bun.get_name.return_value = 'Чужой'
-        mock_bun.get_price.return_value = 10
+        mock_bun.get_name.return_value = Data.bun_name
+        mock_bun.get_price.return_value = Data.ingredient_price_2
         burger = Burger()
         burger.set_buns(mock_bun)
         burger.add_ingredient(mock_ingredient)
@@ -65,12 +66,12 @@ class TestBurger:
 
     def test_get_recept(self):
         mock_ingredient = Mock()
-        type_ingredient = mock_ingredient.get_type.return_value = 'SAUCE'
-        type_ingredient_name = mock_ingredient.get_name.return_value = 'hot sauce'
-        mock_ingredient.get_price.return_value = 20
+        type_ingredient = mock_ingredient.get_type.return_value = Data.ingredient_type
+        type_ingredient_name = mock_ingredient.get_name.return_value = Data.ingredient_name
+        mock_ingredient.get_price.return_value = Data.ingredient_price
         mock_bun = Mock()
-        bun_name = mock_bun.get_name.return_value = 'black bun'
-        mock_bun.get_price.return_value = 10
+        bun_name = mock_bun.get_name.return_value = Data.bun_name
+        mock_bun.get_price.return_value = Data.bun_price
         burger = Burger()
         burger.set_buns(mock_bun)
         burger.add_ingredient(mock_ingredient)
--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -1,22 +1,15 @@
 from praktikum.ingredient import Ingredient
 from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE
+from data import Data
+import pytest
 
 
 class TestIngredient:
-    def test_get_price(self):
-        ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, 'hot sauce', 10)
-        price = ingredient.get_price()
 
-        assert price == 10
+    ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, Data.ingredient_name, Data.ingredient_price_2)
+    @pytest.mark.parametrize("ingredient_method, result",
+                             [(ingredient.get_price(), 10), (ingredient.get_name(), 'hot sauce'), (ingredient.get_type(), 'SAUCE')])
+    def test_get_price(self, ingredient_method, result):
+        ingredient_component = ingredient_method
 
-    def test_get_name(self):
-        ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, 'hot sauce', 10)
-        name = ingredient.get_name()
-
-        assert name == 'hot sauce'
-
-    def test_get_type(self):
-        ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, 'hot sauce', 10)
-        type = ingredient.get_type()
-
-        assert type == 'SAUCE'
+        assert ingredient_component == result
